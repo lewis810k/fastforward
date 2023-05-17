098b59c24de8fb593a254d9fa2b444337c8215e2
김영민 <youngmin.kim@musinsa.com>
2023-05-18 00:06:52 +0900

    데이터 저장

--- src/main/kotlin/com/example/stockchart/entity/StockSummary.kt
+++ src/main/kotlin/com/example/stockchart/entity/StockSummary.kt
@@ -1,13 +1,17 @@
 package com.example.stockchart.entity
 
 import jakarta.persistence.*
+import org.apache.commons.lang3.time.DateUtils
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
 import java.util.Date
 
 @Entity
 @Table(name = "StockSummary")
 data class StockSummary(
         @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-        val id: Int,
+        val id: Int = 0,
 
         @Column
         val symbol: String, // 종목코드
@@ -31,5 +35,5 @@ data class StockSummary(
         val timestamp: Long, // 거래일자
 
         @Column
-        val createdDate: Date, // 생성일시
+        val createdDate: Date = Date.from(Instant.now()), // 생성일시
 )
--- src/main/kotlin/com/example/stockchart/service/StockService.kt
+++ src/main/kotlin/com/example/stockchart/service/StockService.kt
@@ -1,6 +1,8 @@
 package com.example.stockchart.service
 
 import com.example.stockchart.dto.FinanceChartResponse
+import com.example.stockchart.entity.StockSummary
+import com.example.stockchart.repository.StockSummaryRepository
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.http.HttpMethod
 import org.springframework.stereotype.Service
@@ -8,7 +10,9 @@ import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.UriComponentsBuilder
 
 @Service
-class StockService() {
+class StockService(
+        val stockSummaryRepository: StockSummaryRepository
+) {
 
     fun getCharts(): FinanceChartResponse {
         val restTemplate: RestTemplate = RestTemplateBuilder().build()
@@ -17,7 +21,24 @@ class StockService() {
         val body: FinanceChartResponse = restTemplate.exchange(builder.toUriString(), HttpMethod.GET, null, FinanceChartResponse::class.java).body
                 ?: throw Exception()
 
-        println(body)
+        // 해당 날짜 데이터 있으면 반환
+        // 없으면 조회 / 저장 후 반환
+        val meta = body.chart.result[0].meta
+        val timestamps = body.chart.result[0].timestamp
+        val quotes = body.chart.result[0].indicators.quote
+
+        for (i in 0..4) {
+            val stockSummary = StockSummary(
+                    close = quotes[0].close[i],
+                    high = quotes[0].high[i],
+                    low = quotes[0].low[i],
+                    open = quotes[0].open[i],
+                    volume = quotes[0].volume[i],
+                    timestamp = timestamps[i],
+                    symbol = meta.symbol);
+
+            stockSummaryRepository.save(stockSummary)
+        }
 
         return body
     }
